177b9f705851ad69038d95c0128abb3606733c04
Jérôme Alves <j.alves@me.com>
2022-05-23 10:45:51 +0200

    Xcode initial setup

---
+++ SwiftUICameraAppSpeedRun/SwiftUICameraAppSpeedRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICameraAppSpeedRunApp.swift
+//  SwiftUICameraAppSpeedRun
+//
+//  Created by Jérôme Alves on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICameraAppSpeedRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
